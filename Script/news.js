35715bd4a732d60cc35b94e9268164582dca4037
Ganesh-Best <=>
2020-05-12 23:29:34 +0530

    3rd commit

--- Script/news.js
+++ Script/news.js
@@ -38,21 +38,37 @@ News.articles.forEach((article)=>{
 document.querySelector('#collapse').innerHTML = text;
 
 }
+
+//It is insertRow function :  Insert News to Card structure and append to row Object(Element Having id value= collase):
 const insertRow = News =>{
-    
+
+ //creating article variable & assigning empty array/list :  
 const Articles  = [] ;
-console.log("Our news :",News);
+
+//Inserting random article from News.article array/list to Articles List:
 Articles.push(News.articles[random()]);
+
+//Inserting random article from News.article array/list to Articles List:
 Articles.push(News.articles[random()]);
+
+//Inserting random article from News.article array/list to Articles List:
 Articles.push(News.articles[random()]);
 
  
-console.log(Articles);
+//creating text variable & assigning empty string :  
 let text = ``;
+
+//It is looping article list through for Each function():
+//article is array of News
+//article = [ {News1},{News2},{News3}....]:
+//forEach() is built-in method in array/list:
 Articles.forEach((article)=>{
+ 
+ //Calling card function:it return card structure as string ,storing to text variable & giving appropiate arguments : 
  text  += card(article.title,article.url,article.urlToImage)
 });
 
+//Accessing row object(element having class value = row) & assigning text as innerHTML :
 document.querySelector('.row').innerHTML = text ;
  
 }
@@ -62,26 +78,52 @@ document.querySelector('.row').innerHTML = text ;
 // Providing an argument as url(where to fetch news data :)
 const  fetchApi = (url,insertRow,insertCollapse) =>{
 
+ //For request and fetching data from server ,we create Object of XMLHttpRequest Object : 
   let xhr =  new  XMLHttpRequest() ;
+ 
+ //calling open function and passing three argument:
+ //1st one : either GET or POST:
+ //2nd one:  Url:
+ //3rd : either true or false :
+ //true for asynchronous operations:
+ //false for synchronous operations,now we donot use it :  
   xhr.open("GET",url,true);
+
+ //calling send function: Responsible for sending request to given url :
   xhr.send();
   
+ //onprogress is event : It triggers whenever data is loading from server :
+ //assigning an anonymous function to onprogress event :
+ //so when onprogress event triggers :given function will run automatically :  
   xhr.onprogress = () =>{
     let text  = `<h1 class="display-4 text-center mx-auto">Wait a Minute: Data is loading :)</h1> `
     document.querySelector('#row').innerHTML = text; 
     document.querySelector('#collapse').innerHTML = text;
   }
+ //onload is event :It triggers when data completely load from server or response received from server :
+ //assigning an anonymous function to onload event :
+ //so when onload event triggers :given function will run automatically :   
   xhr.onload = () =>{
-
+   //whenever data is succesfully load from server status container value:200
+   //Otherwise it would be 404,503.....etc:
     if(xhr.status == 200){
-      console.log(JSON.parse(xhr.responseText));
+      // xhr.responseText contain  data(data as text) sent from server:
+      //JSON.parse() :responsible to convert JSON text into javascript Object:
+      //So receiving data ,convert into javascript object and storing into News Object :    
        const News = JSON.parse(xhr.responseText) ;
+      //Calling insertRow function and passing News object to it: 
         insertRow(News); 
+      //Calling insertCollapse function and passing News object to it:  
         insertCollapse(News);
     }
     else{
+      // assigning  html string into text vaiable :
       let text = `<h1 class="display-4"> <span class="badge badge-danger">Oops data did not load ,Try again :( </span> </h1>` ;
+      
+      //accessing row object(element having id value = row ) & insert text to it innerHTML :
       document.querySelector('#row').innerHTML = text ;
+
+      //accessing collapse object(element having id value = collapse ) & insert text to it innerHTML :
       document.querySelector('#collapse').innerHTML = text ;
 
     }
